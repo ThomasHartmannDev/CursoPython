14da99786515c1b28bde9acdd18c0c993d9bdccc
Thomas Hartmann <Thomaskuran04@gmail.com>
2019-04-01 23:12:14 +0200

    .

---
+++ Programas/2 - Manipulação de arquivos/io_v4.py
@@ -0,0 +1,11 @@
+
+try: #Tenta executar o codigo dentro dele...
+    arquivo = open('Programas/2 - Manipulação de arquivos/pessoas.csv',encoding="utf-8")
+    for registro in arquivo: 
+        print('Nome: {} ,Idade: {}'.format(*registro.strip().split(',')))
+finally: # Caso de algum erro ou termine a execução do bloco Try, finally será executado.
+    print('Finally executado')
+    arquivo.close()
+    '''
+    Caso o Try de algum erro ele passa pelo finally porem o resto do programa fora do finally não è executado.
+    '''
---
+++ Programas/2 - Manipulação de arquivos/io_v5.py
@@ -0,0 +1,17 @@
+
+try: #Tenta executar o codigo dentro dele...
+    arquivo = open('Programas/2 - Manipulação de arquivos/pessoas.csv',encoding="utf-8")
+    for registro in arquivo: 
+        print('Nome: {} ,Idade: {}'.format(*registro.strip().split(',')))
+
+except IndexError: # o except apenas entra em ação caso de algum erro no try.
+    pass
+    '''
+    o except permite que o resto do codigo seja executado e não parado
+    '''
+finally: # Caso de algum erro ou termine a execução do bloco Try, finally será executado.
+    print('Finally executado')
+    arquivo.close()
+
+if arquivo.close:
+    print('Arquivo fechado com sucesso')
